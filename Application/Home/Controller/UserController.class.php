c8a4af469cb1515ca3b0b7e14f13211be7ddc1aa
MierX <www.1214915547@qq.com>
2019-11-05 18:35:22 +0800

    初始化

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -6,6 +6,21 @@ class UserController extends BaseController {
         if($_GET) {
             $user_info = D($_GET['role']) -> field('*') -> where(['id' => $_GET['id']]) -> find();
         }
+        if($_POST) {
+            if(stristr($_POST['sex'],'女')) {
+                $_POST['sex'] = 0;
+            } else if(stristr($_POST['sex'],'男')) {
+                $_POST['sex'] = 1;
+            } else {
+                unset($_POST['sex']);
+            }
+            $_POST['age'] = intval($_POST['age']);
+            $_POST['work-year'] = intval($_POST['work-year']);
+            $id = $_POST['id'];
+            unset($_POST['id']);
+            D($_POST['role']) -> where(['id' => $id]) -> save($_POST);
+            $user_info = D($_POST['role']) -> field('*') -> where(['id' => $id]) -> find();
+        }
         $this -> user = $user_info;
         $this -> display();
     }
