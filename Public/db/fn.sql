8812f8712fc553a2f3eff54f54d12e732faa4565
MierX <www.1214915547@qq.com>
2019-11-23 15:31:48 +0800

    初始化

--- Public/db/fn.sql
+++ Public/db/fn.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 12/11/2019 19:03:06
+ Date: 23/11/2019 15:30:54
 */
 
 SET NAMES utf8mb4;
@@ -51,7 +51,7 @@ CREATE TABLE `needs`  (
   `endtime` int(10) NOT NULL,
   `status` int(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of needs
@@ -67,6 +67,8 @@ INSERT INTO `needs` VALUES (8, 2, '【重病求医！】帮我买个板蓝根！
 INSERT INTO `needs` VALUES (9, 2, '【重病求医！】帮我买个板蓝根！', '感冒', 20, 5, '<p>帮我买盒板蓝根！</p><p><img src=\"/ueditor/php/upload/image/20191111/1573457078877817.jpg\" title=\"1573457078877817.jpg\" alt=\"chan.jpg\"/></p>', 1573457204, 1573457204, 4);
 INSERT INTO `needs` VALUES (10, 2, 'aaa！', '感冒', 20, 5, '<p>帮我买盒板蓝根！</p><p><img src=\"/ueditor/php/upload/image/20191111/1573457078877817.jpg\" title=\"1573457078877817.jpg\" alt=\"chan.jpg\"/></p>', 1573457207, 1574062007, 1);
 INSERT INTO `needs` VALUES (11, 2, '【重病求医！】帮我买个板蓝根！', '感冒', 20, 5, '<p>帮我买盒板蓝根！</p><p><img src=\"/ueditor/php/upload/image/20191111/1573457078877817.jpg\" title=\"1573457078877817.jpg\" alt=\"chan.jpg\"/></p>', 1573457277, 1573457277, 4);
+INSERT INTO `needs` VALUES (12, 3, 'wo shi xin lai de ', 'wuliao', 12, 1, '<p>超级无聊boring</p>', 1573612216, 1574217016, 1);
+INSERT INTO `needs` VALUES (13, 2, 'test', 'wuliao', 24, 24, '<p>23232321231344</p>', 1573614408, 1573457087, 4);
 
 -- ----------------------------
 -- Table structure for notice
@@ -86,16 +88,16 @@ CREATE TABLE `notice`  (
 -- ----------------------------
 -- Records of notice
 -- ----------------------------
-INSERT INTO `notice` VALUES (1, 'test1', 5, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
+INSERT INTO `notice` VALUES (1, 'test1', 6, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (2, 'test2', 4, '管理员', '<p>test22222222</p>', 1, 1573056000);
-INSERT INTO `notice` VALUES (3, 'test3', 40, '管理员', '<p>请新编辑内容</p>', 1, 1573117385);
+INSERT INTO `notice` VALUES (3, 'test3', 42, '管理员', '<p>请新编辑内容</p>', 1, 1573117385);
 INSERT INTO `notice` VALUES (4, 'test1', 10, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (5, 'test1', 7, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (6, 'test1', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (7, 'test1', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (8, 'test1', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (9, 'test1', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
-INSERT INTO `notice` VALUES (10, 'test1', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
+INSERT INTO `notice` VALUES (10, 'test1', 5, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 INSERT INTO `notice` VALUES (11, 'test11', 1, '管理员', '<p>test1</p><p><span style=\"font-size: 20px;\">test1</span></p><p><span style=\"font-size: 24px;\">test1</span></p><p><span style=\"font-size: 24px;\"><img src=\"/ueditor/php/upload/image/20191107/1573107649761427.gif\" title=\"1573107649761427.gif\" alt=\"ball.gif\"/></span></p>', 1, 1522101156);
 
 -- ----------------------------
@@ -120,13 +122,38 @@ CREATE TABLE `nurse`  (
   `status` int(1) NOT NULL DEFAULT 1,
   `addtime` int(10) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of nurse
 -- ----------------------------
 INSERT INTO `nurse` VALUES (1, 'Nurse', 'test001', '123456', 'test1', 1, 18, 12312312300, 12345678900987654, '2', 'qwadasd', NULL, 0, 'asdasdasdaf', 1, 1572687086);
-INSERT INTO `nurse` VALUES (2, 'Nurse', 'test002', '123456', 'test2', 0, 41, 12312312301, 123123123123123123, '15', '123213', NULL, 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (2, 'Nurse', 'test002', '123456', 'test2', 2, 41, 12312312301, 123123123123123123, '15', '123213', NULL, 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (3, 'Nurse', 'test003', '123456', 'test3', 1, 41, 12312312302, 123123123123123124, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (4, 'Nurse', 'test004', '123456', 'test4', 2, 41, 12312312303, 123123123123123125, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (5, 'Nurse', 'test005', '123456', 'test5', 1, 41, 12312312304, 123123123123123126, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (6, 'Nurse', 'test006', '123456', 'test6', 2, 41, 12312312305, 123123123123123127, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (7, 'Nurse', 'test007', '123456', 'test7', 1, 41, 12312312306, 123123123123123128, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (8, 'Nurse', 'test008', '123456', 'test8', 2, 41, 12312312307, 123123123123123129, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (9, 'Nurse', 'test009', '123456', 'test9', 1, 41, 12312312308, 1231231231231231210, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (10, 'Nurse', 'test010', '123456', 'test10', 2, 41, 12312312309, 1231231231231231211, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+INSERT INTO `nurse` VALUES (11, 'Nurse', 'test011', '123456', 'test11', 1, 41, 12312312310, 1231231231231231212, '10', '123213', '感冒', 0, 'testtest', 1, 1572687086);
+
+-- ----------------------------
+-- Table structure for nurse_collection
+-- ----------------------------
+DROP TABLE IF EXISTS `nurse_collection`;
+CREATE TABLE `nurse_collection`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nid` int(11) NOT NULL,
+  `ids` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of nurse_collection
+-- ----------------------------
+INSERT INTO `nurse_collection` VALUES (8, 2, '{\"1\":8,\"2\":7,\"3\":13,\"4\":12,\"5\":1,\"6\":2}');
 
 -- ----------------------------
 -- Table structure for user
@@ -145,13 +172,14 @@ CREATE TABLE `user`  (
   `status` int(1) NOT NULL DEFAULT 1,
   `addtime` int(10) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'User', 'test001', '123456', 'test1', 1, 18, 12312312300, 12345678900987654, 2, 1572687034);
+INSERT INTO `user` VALUES (1, 'User', 'test001', '123456', 'test1', 1, 18, 12312312300, 12345678900987654, 1, 1572687034);
 INSERT INTO `user` VALUES (2, 'User', 'test002', '123456', 'test2', 1, 27, 12312312301, 123123123123123123, 1, 1572687034);
+INSERT INTO `user` VALUES (3, 'User', 'test003', '123456', 'test3', 1, 32, 12312312302, 123123123123123132, 1, 1573612148);
 
 -- ----------------------------
 -- Table structure for user_collection
@@ -162,7 +190,7 @@ CREATE TABLE `user_collection`  (
   `uid` int(11) NOT NULL,
   `ids` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_collection
